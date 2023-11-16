e84851505769a44c2ea8c0d5d3a2181185129864
ashrith-25 <99070646+ashrith-25@users.noreply.github.com>
2023-11-16 16:13:00 +0530

    Delete BookIT_BackEnd-main/BookIT_BackEnd-main directory

--- BookIT_BackEnd-main/BookIT_BackEnd-main/database/connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const connect = async () => {
-    await mongoose.connect('mongodb+srv://SeetaRam217:mernstack@mern-project.cjeobsp.mongodb.net/?retryWrites=true&w=majority')
-    .then(() => console.log('MongoDB connection is SuccessFul'))
-    .catch(err => console.log(err))
-}
-
-module.exports = connect
